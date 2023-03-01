c9fac5241b81e4f1473e06614f6fe1508d36d702
Wai-Shing Luk <luk036@gmail.com>
2023-03-01 22:01:20 +0800

    fix the build; apply fix-format

--- specific.cmake
+++ specific.cmake
@@ -1,32 +1,26 @@
-cpmaddpackage(
-  NAME
-  fmt
-  GIT_TAG
-  7.1.3
-  GITHUB_REPOSITORY
-  fmtlib/fmt
-  OPTIONS
-  "FMT_INSTALL YES" # create an installable target
+CPMAddPackage(
+  NAME fmt
+  GIT_TAG 7.1.3
+  GITHUB_REPOSITORY fmtlib/fmt
+  OPTIONS "FMT_INSTALL YES" # create an installable target
 )
 
-cpmaddpackage("gh:microsoft/GSL@3.1.0")
+CPMAddPackage("gh:microsoft/GSL@3.1.0")
 
-cpmaddpackage(
-  NAME
-  Lds
-  GIT_TAG
-  1.0.4
-  GITHUB_REPOSITORY
-  luk036/lds-cpp
+CPMAddPackage(
+  NAME Lds
+  GIT_TAG 1.0.4
+  GITHUB_REPOSITORY luk036/lds-cpp
+  OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
 
-cpmaddpackage("gh:xtensor-stack/xtl#0.6.23")
+CPMAddPackage("gh:xtensor-stack/xtl#0.6.23")
 if(xtl_ADDED)
   message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
   include_directories(${xtl_SOURCE_DIR}/include)
 endif(xtl_ADDED)
 
-cpmaddpackage("gh:xtensor-stack/xtensor#0.22.0")
+CPMAddPackage("gh:xtensor-stack/xtensor#0.22.0")
 if(xtensor_ADDED)
   message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
   include_directories(${xtensor_SOURCE_DIR}/include)
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,55 +1,55 @@
-#include <sphere_n/greeter.h>  // for Sphere_N
-#include <sphere_n/version.h>  // for SPHERE_N_VERSION
+#include <sphere_n/greeter.h> // for Sphere_N
+#include <sphere_n/version.h> // for SPHERE_N_VERSION
 
-#include <cxxopts.hpp>    // for value, OptionAdder, Options, OptionValue
-#include <iostream>       // for string, operator<<, endl, basic_ostream
-#include <memory>         // for shared_ptr
-#include <string>         // for char_traits, hash, operator==
-#include <unordered_map>  // for operator==, unordered_map, __hash_map_const...
-#include <utility>        // for pair
+#include <cxxopts.hpp>   // for value, OptionAdder, Options, OptionValue
+#include <iostream>      // for string, operator<<, endl, basic_ostream
+#include <memory>        // for shared_ptr
+#include <string>        // for char_traits, hash, operator==
+#include <unordered_map> // for operator==, unordered_map, __hash_map_const...
+#include <utility>       // for pair
 
-auto main(int argc, char** argv) -> int {
-    const std::unordered_map<std::string, sphere_n::LanguageCode> languages{
-        {"en", sphere_n::LanguageCode::EN},
-        {"de", sphere_n::LanguageCode::DE},
-        {"es", sphere_n::LanguageCode::ES},
-        {"fr", sphere_n::LanguageCode::FR},
-    };
+auto main(int argc, char **argv) -> int {
+  const std::unordered_map<std::string, sphere_n::LanguageCode> languages{
+      {"en", sphere_n::LanguageCode::EN},
+      {"de", sphere_n::LanguageCode::DE},
+      {"es", sphere_n::LanguageCode::ES},
+      {"fr", sphere_n::LanguageCode::FR},
+  };
 
-    cxxopts::Options options(*argv, "A program to welcome the world!");
+  cxxopts::Options options(*argv, "A program to welcome the world!");
 
-    std::string language;
-    std::string name;
+  std::string language;
+  std::string name;
 
-    // clang-format off
+  // clang-format off
   options.add_options()
     ("h,help", "Show help")
     ("v,version", "Print the current version number")
     ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
     ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
   ;
-    // clang-format on
+  // clang-format on
 
-    auto result = options.parse(argc, argv);
+  auto result = options.parse(argc, argv);
 
-    if (result["help"].as<bool>()) {
-        std::cout << options.help() << std::endl;
-        return 0;
-    }
-
-    if (result["version"].as<bool>()) {
-        std::cout << "Sphere_N, version " << SPHERE_N_VERSION << std::endl;
-        return 0;
-    }
-
-    auto langIt = languages.find(language);
-    if (langIt == languages.end()) {
-        std::cerr << "unknown language code: " << language << std::endl;
-        return 1;
-    }
-
-    // sphere_n::Sphere_N sphere_n(name);
-    // std::cout << sphere_n.greet(langIt->second) << std::endl;
-
+  if (result["help"].as<bool>()) {
+    std::cout << options.help() << std::endl;
     return 0;
+  }
+
+  if (result["version"].as<bool>()) {
+    std::cout << "Sphere_N, version " << SPHERE_N_VERSION << std::endl;
+    return 0;
+  }
+
+  auto langIt = languages.find(language);
+  if (langIt == languages.end()) {
+    std::cerr << "unknown language code: " << language << std::endl;
+    return 1;
+  }
+
+  // sphere_n::Sphere_N sphere_n(name);
+  // std::cout << sphere_n.greet(langIt->second) << std::endl;
+
+  return 0;
 }
