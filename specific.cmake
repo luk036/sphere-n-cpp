2627f8fe06e58ebb3881f94b125cb5fe92b595b6
Wai-Shing Luk <luk036@gmail.com>
2023-03-01 21:52:03 +0800

    separate lds and sphere-n

---
+++ specific.cmake
@@ -0,0 +1,36 @@
+cpmaddpackage(
+  NAME
+  fmt
+  GIT_TAG
+  7.1.3
+  GITHUB_REPOSITORY
+  fmtlib/fmt
+  OPTIONS
+  "FMT_INSTALL YES" # create an installable target
+)
+
+cpmaddpackage("gh:microsoft/GSL@3.1.0")
+
+cpmaddpackage(
+  NAME
+  Lds
+  GIT_TAG
+  1.0.4
+  GITHUB_REPOSITORY
+  luk036/lds-cpp
+)
+
+cpmaddpackage("gh:xtensor-stack/xtl#0.6.23")
+if(xtl_ADDED)
+  message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
+  include_directories(${xtl_SOURCE_DIR}/include)
+endif(xtl_ADDED)
+
+cpmaddpackage("gh:xtensor-stack/xtensor#0.22.0")
+if(xtensor_ADDED)
+  message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
+  include_directories(${xtensor_SOURCE_DIR}/include)
+endif(xtensor_ADDED)
+
+set(SPECIFIC_LIBS fmt::fmt GSL Lds::Lds)
+# remember to turn off the warnings
