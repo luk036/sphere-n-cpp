79acc4ed0cc253f259bad88614037c777ac77492
Wai-Shing Luk <luk036@gmail.com>
2024-06-26 11:43:21 +0000

    update regression according to Circle change

--- test/source/test_lds_n.cpp
+++ test/source/test_lds_n.cpp
@@ -8,7 +8,7 @@ TEST_CASE("Sphere3") {
     const size_t base[] = {2, 3, 5};
     auto sp3gen = lds2::Sphere3(base);
     const auto res = sp3gen.pop();
-    CHECK_EQ(res[0], doctest::Approx(0.0));
+    CHECK_EQ(res[1], doctest::Approx(0.0));
 }
 
 // TEST_CASE("HaltonN") {
@@ -22,12 +22,12 @@ TEST_CASE("CylinN") {
     const size_t base[] = {2, 3, 5, 7};
     auto cygen = lds2::CylinN(base);
     const auto res = cygen.pop();
-    CHECK_EQ(res[0], doctest::Approx(0.5896942325));
+    CHECK_EQ(res[1], doctest::Approx(0.5896942325));
 }
 
 TEST_CASE("SphereN") {
     const size_t base[] = {2, 3, 5, 7};
     auto spgen = lds2::SphereN(base);
     const auto res = spgen.pop();
-    CHECK_EQ(res[0], doctest::Approx(0.503547));
+    CHECK_EQ(res[1], doctest::Approx(0.503547));
 }
