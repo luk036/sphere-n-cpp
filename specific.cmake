9ded92779d7b370e9d5c6073ed3723ea149fe57a
Wai-Shing Luk <luk036@gmail.com>
2026-07-15 10:35:13 +0800

    chore: bump version 1.1.2 -> 1.1.3, update LdsGen tag v1.2.4

--- specific.cmake
+++ specific.cmake
@@ -7,7 +7,7 @@ CPMAddPackage(
 
 CPMAddPackage(
   NAME LdsGen
-  GIT_TAG 1.2.2
+  GIT_TAG v1.2.4
   GITHUB_REPOSITORY luk036/lds-gen-cpp
   OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
